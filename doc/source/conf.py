f2d93a8915bbf4d91d81bd953d56d0616e8dac83
Dirk Mueller <dirk@dmllr.de>
2015-12-09 17:26:41 +0100

    Update from global requirements
    Change-Id: I43fc7c08b1d6f36b406ae20740c5c55d76a894a7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -103,12 +103,12 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# -- Options for HTML output --------------------------------------------------
 
-# -- Options for HTML output ----------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'alabaster'
+# The theme to use for HTML and HTML Help pages.  Major themes that come with
+# Sphinx are currently 'default' and 'sphinxdoc'.
+# html_theme_path = ["."]
+# html_theme = '_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
