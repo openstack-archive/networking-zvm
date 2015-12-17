087f464289927d80d1198387253c221d35a69d53
Song Qian Xia <qsong@songs-mbp.cn.ibm.com>
2015-12-17 16:43:43 +0800

    Change the copyright of mech_zvm.py. Make the copy right contnent same as other zvm driver file.
    Change-Id: I54bf9f59ebdfdb74b0e30d3da2de61e8c0573a4e

--- neutron/plugins/ml2/drivers/zvm/mech_zvm.py
+++ neutron/plugins/ml2/drivers/zvm/mech_zvm.py
@@ -1,11 +1,5 @@
-# =================================================================
-# Licensed Materials - Property of IBM
+# Copyright 2014 IBM Corp.
 #
-# (c) Copyright IBM Corp. 2014, 2015 All Rights Reserved
-#
-# US Government Users Restricted Rights - Use, duplication or
-# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-# =================================================================
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
