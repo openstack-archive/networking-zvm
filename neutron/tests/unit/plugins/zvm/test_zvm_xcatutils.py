b987ee3c77e4104380106fea486567b1b0bcef00
songqianxia <xqianxia@cn.ibm.com>
2016-04-26 17:27:40 +0800

    Make neutron-zvm-agent exception enhance
    To avoid neutron-zvm-agent pop up grammar error.
    
    Change-Id: I99e583552e893999c5205ee090e65f11d3190a0e

--- neutron/tests/unit/plugins/zvm/test_zvm_xcatutils.py
+++ neutron/tests/unit/plugins/zvm/test_zvm_xcatutils.py
@@ -19,6 +19,7 @@ Unit tests for the z/VM xCAT utils.
 import mock
 from oslo_config import cfg
 
+from neutron.plugins.zvm.common import exception
 from neutron.plugins.zvm.common import xcatutils
 from neutron.tests import base
 
@@ -37,3 +38,21 @@ class TestZVMXcatUtils(base.BaseTestCase):
         with mock.patch.multiple(xcatutils.httplib,
             HTTPSConnection=mock.MagicMock()):
             self._zvm_xcat_connection = xcatutils.xCatConnection()
+
+    def test_restapi_command_return_error(self):
+        fake_method = "GET"
+        fake_url = "fake"
+        fake_body = "fake"
+        fake_messages = ('{"data":[{"data":'
+                   '["zhcp: ERROR: Unsupported API function name",null]},'
+                   '{"errorcode":["1"]}]}')
+        try:
+            with mock.patch.object(xcatutils, "LOG") as log:
+                xcatutils.load_xcat_resp(fake_method, fake_url,
+                                        fake_body, fake_messages)
+                log.error.assert_called_with("Error returned from xCAT: %s",
+                                          fake_messages)
+        except Exception:
+            self.assertRaises(exception.zVMInvalidxCatResponseDataError,
+                              xcatutils.load_xcat_resp, fake_method, fake_url,
+                              fake_body, fake_messages)
