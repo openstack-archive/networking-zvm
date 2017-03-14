915d0a0bd26e6bc53711305d1903efad956b92cc
jichenjc <jichenjc@cn.ibm.com>
2017-03-14 18:32:40 +0800

    Adopt community changes
    change from
    from neutron.agent.common import config
    to
    from neutron.conf.agent import common as config
    
    Change-Id: I418ecbceed905f72bcbf09930530020f94a5e7f5

--- neutron/plugins/zvm/common/config.py
+++ neutron/plugins/zvm/common/config.py
@@ -17,8 +17,7 @@
 from oslo_config import cfg
 
 from neutron._i18n import _
-from neutron.agent.common import config
-
+from neutron.conf.agent import common as config
 
 agent_opts = [
     cfg.StrOpt(
